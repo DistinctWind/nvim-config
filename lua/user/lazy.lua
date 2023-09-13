11249b077d29f44ca58d01912411be08f227c51f
rankrev <zq_zhouqi@qq.com>
2023-09-13 23:15:08 +0800

    use structured lazy plugin config | add tokyonight theme

--- lua/user/lazy.lua
+++ lua/user/lazy.lua
@@ -12,8 +12,4 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({
-    "folke/which-key.nvim",
-    { "folke/neoconf.nvim", cmd = "Neoconf" },
-    "folke/neodev.nvim",
-})
+require("lazy").setup("user.plugins")
