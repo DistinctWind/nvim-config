2551d332ee114c7381d9918083fdcbaa357b4ef7
Gorun <307532500@qq.com>
2023-09-15 11:43:59 +0800

    add settings for lua_ls

--- lua/gorun/plugins/lsp/lspconfig.lua
+++ lua/gorun/plugins/lsp/lspconfig.lua
@@ -4,15 +4,16 @@ return {
     event = { "BufReadPre", "BufNewFile" },
     dependencies = {
         "hrsh7th/cmp-nvim-lsp",
+        "folke/neodev.nvim",
     },
     config = function()
         local lspconfig = require("lspconfig")
         local cmp_nvim_lsp = require("cmp_nvim_lsp")
         local builtin = require("telescope.builtin")
+        require("neodev").setup()
 
-        local capabilities = cmp_nvim_lsp.capabilities
+        local capabilities = cmp_nvim_lsp.default_capabilities()
         local keymap = vim.keymap.set
-        require("gorun.keymaps")
         local on_attach = function(_, bufnr)
             local desc = function (str)
                 local opts = desc(str)
@@ -25,7 +26,19 @@ return {
 
         lspconfig["lua_ls"].setup {
             capabilities = capabilities,
-            on_attach = on_attach
+            on_attach = on_attach,
+            settings = {
+                Lua = {
+                    runtime = {
+                        -- Tell the language server which version of Lua you're using
+                        -- (most likely LuaJIT in the case of Neovim)
+                        version = 'LuaJIT'
+                    },
+                    workspace = {
+                        checkThirdParty = false,
+                    }
+                }
+            }
         }
     end
 }
