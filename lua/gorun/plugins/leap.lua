4030d50d02f8c8cf92109b3380daa4f863e1976e
rankrev <zq_zhouqi@qq.com>
2023-09-20 11:24:20 +0800

    make leap convenient keymap

--- lua/gorun/plugins/leap.lua
+++ lua/gorun/plugins/leap.lua
@@ -7,12 +7,17 @@ return {
     config = function ()
         local keymap = vim.keymap.set
         local leap = require("leap")
-        keymap("n", "<leader>j", function()
+        local function jump_current_window()
             leap.leap{ target_windows = { vim.fn.win_getid() } }
-        end, desc "[J]ump")
-        keymap("n", "<leader>J", function()
+        end
+
+        local function jump_global_window()
             local windows = require("leap.util").get_enterable_windows()
             table.insert(windows, vim.fn.win_getid())
             leap.leap{ target_windows = windows }
-        end, desc "[J]ump All Windows")
+        end
+        keymap("n", "<M-j>", jump_current_window, desc "[J]ump")
+        keymap("n", "<leader>j", jump_current_window, desc "[J]ump")
+        keymap("n", "<M-k>", jump_global_window, desc "[J]ump All Windows")
+        keymap("n", "<leader>J", jump_global_window, desc "[J]ump All Windows")
     end }
