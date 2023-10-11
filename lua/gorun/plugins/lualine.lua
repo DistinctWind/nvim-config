8220ec52b5153e88ef81258f0b9a7fdbf6361983
rankrev <zq_zhouqi@qq.com>
2023-10-11 16:22:19 +0800

    add config function for lualine init

--- lua/gorun/plugins/lualine.lua
+++ lua/gorun/plugins/lualine.lua
@@ -1,52 +1,56 @@
 return {
     'nvim-lualine/lualine.nvim',
     dependencies = { "nvim-tree/nvim-web-devicons" },
-    opts = {
-        options = {
-            icons_enabled = true,
-            theme = 'auto',
-            section_separators = { left = '', right = ''},
-            component_separators = { left = '', right = ''},
-            disabled_filetypes = {
-                statusline = {},
-                winbar = {},
-                "NvimTree",
-                "qf",
-            },
-            ignore_focus = {},
-            always_divide_middle = true,
-            globalstatus = false,
-            refresh = {
-                statusline = 1000,
-                tabline = 1000,
-                winbar = 1000,
-            }
-        },
-        sections = {
-            lualine_a = {'mode'},
-            lualine_b = {'branch'},
-            lualine_c = {'filename'},
-            lualine_x = {'encoding', 'filetype'},
-            lualine_y = {},
-            lualine_z = {
-                {
-                    "location",
-                    icons_enabled = true,
-                    icon = ""
+    event = "VeryLazy",
+    config = function()
+        local lualine = require("lualine")
+        lualine.setup {
+            options = {
+                icons_enabled = true,
+                theme = 'auto',
+                section_separators = { left = '', right = ''},
+                component_separators = { left = '', right = ''},
+                disabled_filetypes = {
+                    statusline = {},
+                    winbar = {},
+                    "NvimTree",
+                    "qf",
                 },
-            }
-        },
-        inactive_sections = {
-            lualine_a = {},
-            lualine_b = {},
-            lualine_c = {'filename'},
-            lualine_x = {'location'},
-            lualine_y = {},
-            lualine_z = {}
-        },
-        tabline = {},
-        winbar = {},
-        inactive_winbar = {},
-        extensions = {}
-    }
+                ignore_focus = {},
+                always_divide_middle = true,
+                globalstatus = false,
+                refresh = {
+                    statusline = 1000,
+                    tabline = 1000,
+                    winbar = 1000,
+                }
+            },
+            sections = {
+                lualine_a = {'mode'},
+                lualine_b = {'branch'},
+                lualine_c = {'filename'},
+                lualine_x = {'encoding', 'filetype'},
+                lualine_y = {},
+                lualine_z = {
+                    {
+                        "location",
+                        icons_enabled = true,
+                        icon = ""
+                    },
+                }
+            },
+            inactive_sections = {
+                lualine_a = {},
+                lualine_b = {},
+                lualine_c = {'filename'},
+                lualine_x = {'location'},
+                lualine_y = {},
+                lualine_z = {}
+            },
+            tabline = {},
+            winbar = {},
+            inactive_winbar = {},
+            extensions = {}
+        }
+    end
 }
