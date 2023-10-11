7e3d182294b20cc8305c4d2c7aab52958a944bbf
rankrev <zq_zhouqi@qq.com>
2023-10-11 11:51:13 +0800

    add swap text objects

--- lua/gorun/plugins/treesitter.lua
+++ lua/gorun/plugins/treesitter.lua
@@ -49,7 +49,16 @@ return {
                         ["[M"] = "@function.outer",
                         ["[]"] = "@class.outer",
                     },
-                }
+                },
+                swap = {
+                    enable = true,
+                    swap_next = {
+                        ["<leader>a"] = "@parameter.inner",
+                    },
+                    swap_previous = {
+                        ["<leader>A"] = "@parameter.inner",
+                    },
+                },
             }
         })
     end
