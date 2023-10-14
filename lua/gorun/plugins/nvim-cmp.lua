d475519aba2879d3a98ecb69c073b2baa73a26e2
rankrev <zq_zhouqi@qq.com>
2023-10-14 23:59:58 +0800

    add autopair plugin and cmp integration

--- lua/gorun/plugins/nvim-cmp.lua
+++ lua/gorun/plugins/nvim-cmp.lua
@@ -61,5 +61,11 @@ return {
                 },
             },
         })
+
+        local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+        cmp.event:on(
+            "confirm_done",
+            cmp_autopairs.on_confirm_done()
+        )
     end,
 }
