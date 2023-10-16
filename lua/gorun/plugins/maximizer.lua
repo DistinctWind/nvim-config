0a599b91a08b0ea6fd0224ac5a2935a44226535d
rankrev <zq_zhouqi@qq.com>
2023-10-16 11:07:25 +0800

    remove <c-o> maximizer keymap

--- lua/gorun/plugins/maximizer.lua
+++ lua/gorun/plugins/maximizer.lua
@@ -1,9 +1,11 @@
+-- Config maximizer plugin before it loads
+vim.g.maximizer_set_default_mapping = 1
+
 return {
     event = "VeryLazy",
     "szw/vim-maximizer",
     config = function ()
         local keymap = vim.keymap.set
         local desc = require("gorun.keymaps").desc
-        keymap("n", "<C-o>", ":MaximizerToggle<CR>", desc "[O]verfill the window")
     end
 }
